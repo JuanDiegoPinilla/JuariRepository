ad4d8b7d2e1c8e8289fdef5453b7c063f6cb39a9
arisocode <aricabrerac21@gmail.com>
2026-08-09 22:12:57 -0500

    Se crean carpetas de orden springboot y se desarrolla lo pedido en el PBI scrum 1

---
+++ store/store/src/main/java/com/juari/store/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.juari.store.repository;
+
+import com.juari.store.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
